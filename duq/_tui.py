0ce4e7c37d1fd5c0a5f70cab37a596f0024ac6f2
Brandon Pickering <brandonpickering95@gmail.com>
2026-09-05 10:02:39 -0700

    Add other auto-close handlers

--- duq/_tui.py
+++ duq/_tui.py
@@ -34,24 +34,51 @@ def run_tui():
     def _(event: KeyPressEvent) -> None:
         event.app.exit()
 
-    @Condition
-    def is_in_empty_braces() -> bool:
-        text = source_buffer.text
-        pos = source_buffer.cursor_position
-        if pos <= 0 or pos >= len(text):
-            return False
-        pair = text[pos - 1 : pos + 1]
-        return pair == "()"
+    def define_brace_handlers(brace_pair: str) -> None:
+        lbrace = brace_pair[0]
+        rbrace = brace_pair[1]
 
-    @key_bindings.add("backspace", filter=is_in_empty_braces)
-    def _(event: KeyPressEvent) -> None:
-        event.current_buffer.cursor_right()
-        event.current_buffer.delete_before_cursor(2)
+        @Condition
+        def should_auto_close() -> bool:
+            text = source_buffer.text
+            pos = source_buffer.cursor_position
+            return pos >= 1 and text[pos - 1] != "\\"
 
-    @key_bindings.add("(")
-    def _(event: KeyPressEvent) -> None:
-        event.current_buffer.insert_text("()")
-        event.current_buffer.cursor_left()
+        @Condition
+        def should_eat_closing_brace() -> bool:
+            text = source_buffer.text
+            pos = source_buffer.cursor_position
+            if pos >= 1 and text[pos - 1] == "\\":
+                return False
+            if pos >= len(text):
+                return False
+            next = text[pos]
+            return next == rbrace
+
+        @Condition
+        def is_in_empty_braces() -> bool:
+            text = source_buffer.text
+            pos = source_buffer.cursor_position
+            if pos <= 0 or pos >= len(text):
+                return False
+            return text[pos - 1 : pos + 1] == brace_pair
+
+        @key_bindings.add(lbrace, filter=should_auto_close)
+        def _(event: KeyPressEvent) -> None:
+            event.current_buffer.insert_text(brace_pair)
+            event.current_buffer.cursor_left()
+
+        @key_bindings.add(rbrace, filter=should_eat_closing_brace)
+        def _(event: KeyPressEvent) -> None:
+            event.current_buffer.cursor_right()
+
+        @key_bindings.add("backspace", filter=is_in_empty_braces)
+        def _(event: KeyPressEvent) -> None:
+            event.current_buffer.cursor_right()
+            event.current_buffer.delete_before_cursor(2)
+
+    for brace_pair in ["()", "[]", "{}", '""', "''"]:
+        define_brace_handlers(brace_pair)
 
     def on_source_changed(source_buffer: Buffer) -> None:
         source = source_buffer.text
